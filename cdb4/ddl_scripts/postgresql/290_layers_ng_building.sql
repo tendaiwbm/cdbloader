be8864cb478d91f15c2dcc9e79524df4a05d0a0c
tendaiwbm <t.mbwanda@student.tudelft.nl>
2023-07-08 19:46:00 +0200

    test

--- cdb4/ddl_scripts/postgresql/290_layers_ng_building.sql
+++ cdb4/ddl_scripts/postgresql/290_layers_ng_building.sql
@@ -7,7 +7,7 @@
 --
 -- Delft University of Technology, The Netherlands
 -- 3D Geoinformation Group
--- https://3d.bk.tudelft.nl/
+-- https://3d.bk.tudelft.nl
 -- 
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
